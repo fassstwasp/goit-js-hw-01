f4d64ea54bb9c1cae454c499437e617cd89a8ca2
Arthur Pepentcev <fastwasp12@gmail.com>
2024-10-08 15:06:19 +0300

    js

--- task-1.js
+++ task-1.js
@@ -2,5 +2,3 @@ function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-
-console.log(makeTransaction(5, 3000));
